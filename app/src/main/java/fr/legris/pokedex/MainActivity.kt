646bc014380c2122500a2d456bfaf62a6f1ed5b9
Nico <legris.nicolas@outlook.com>
2021-03-10 11:36:11 +0100

    init

---
+++ app/src/main/java/fr/legris/pokedex/MainActivity.kt
@@ -0,0 +1,18 @@
+package fr.legris.pokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.legris.pokedex.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
