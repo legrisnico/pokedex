3c7d4d467146f9352432660b5c05fea2e2dc7e0f
Nico <legris.nicolas@outlook.com>
2021-11-23 22:25:00 +0100

    remove light and dark types colors

--- app/src/main/java/fr/legris/pokedex/ui/pokemondetail/PokemonDetailView.kt
+++ app/src/main/java/fr/legris/pokedex/ui/pokemondetail/PokemonDetailView.kt
@@ -4,9 +4,6 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyGridScope
-import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,14 +23,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import coil.request.CachePolicy
-import coil.transform.CircleCropTransformation
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import fr.legris.pokedex.R
 import fr.legris.pokedex.ui.model.Pokemon
 import fr.legris.pokedex.ui.model.Type
-import fr.legris.pokedex.ui.pokemonlist.Pokemon
-import fr.legris.pokedex.ui.theme.PokedexTheme
-import fr.legris.pokedex.ui.theme.electricTypeColorDark
 import fr.legris.pokedex.utils.Resource
 
 @ExperimentalFoundationApi
@@ -56,10 +48,11 @@ fun PokemonDetailView(
 
             }
             Resource.Status.SUCCESS -> {
-                if(pokemon?.data == null){
+                if (pokemon?.data == null) {
                     ErrorSnackBar(
                         scaffoldState = scaffoldState,
-                        message = pokemon?.message ?: stringResource(R.string.error_pokemon_detail_default)
+                        message = pokemon?.message
+                            ?: stringResource(R.string.error_pokemon_detail_default)
                     )
                 }
                 PokemonDetail(pokemon = pokemon?.data!!)
@@ -67,7 +60,8 @@ fun PokemonDetailView(
             Resource.Status.ERROR -> {
                 ErrorSnackBar(
                     scaffoldState = scaffoldState,
-                    message = pokemon?.message ?: stringResource(R.string.error_pokemon_detail_default)
+                    message = pokemon?.message
+                        ?: stringResource(R.string.error_pokemon_detail_default)
                 )
             }
 
@@ -136,7 +130,7 @@ fun ErrorSnackBar(scaffoldState: ScaffoldState, message: String) {
 @ExperimentalFoundationApi
 @ExperimentalCoilApi
 @Composable
-fun PokemonDetail(pokemon: Pokemon){
+fun PokemonDetail(pokemon: Pokemon) {
 
     val scrollState = rememberScrollState()
 
@@ -155,30 +149,25 @@ fun PokemonDetail(pokemon: Pokemon){
         animationSpec = tween(durationMillis = 200)
     )
 
-    if(pokemon.types.isNotEmpty()) {
+    if (pokemon.types.isNotEmpty()) {
         rememberSystemUiController()
             .setSystemBarsColor(color = pokemon.types[0].typeColor)
     }
 
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()
-        .background(animatedBackgroundColor.value)) {
-        Column(
-            modifier = Modifier
-                .verticalScroll(scrollState)
-                .padding(8.dp)
-                .fillMaxWidth()
-        ) {
-            PokemonDetailName(pokemonName = pokemon.name)
-            PokemonDetailGlobalInfos(pokemon = pokemon)
-        }
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        PokemonDetailName(pokemonName = pokemon.name)
+        PokemonDetailGlobalInfos(pokemon = pokemon)
     }
 
 }
 
 @Composable
-fun PokemonDetailName(pokemonName : String){
+fun PokemonDetailName(pokemonName: String) {
     Text(
         modifier = Modifier
             .fillMaxWidth()
@@ -192,7 +181,7 @@ fun PokemonDetailName(pokemonName : String){
 @ExperimentalFoundationApi
 @ExperimentalCoilApi
 @Composable
-fun PokemonDetailGlobalInfos(pokemon: Pokemon){
+fun PokemonDetailGlobalInfos(pokemon: Pokemon) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -219,7 +208,7 @@ fun PokemonDetailGlobalInfos(pokemon: Pokemon){
 
 @ExperimentalFoundationApi
 @Composable
-fun PokemonDetailCharacteristics(pokemon : Pokemon){
+fun PokemonDetailCharacteristics(pokemon: Pokemon) {
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(text = pokemon.height.toString())
         Text(text = pokemon.weight.toString())
@@ -231,7 +220,7 @@ fun PokemonDetailCharacteristics(pokemon : Pokemon){
 }
 
 @Composable
-fun PokemonDetailTypeItem(type : Type){
+fun PokemonDetailTypeItem(type: Type) {
     Text(text = type.typeName)
 }
 
--- app/src/main/java/fr/legris/pokedex/ui/theme/Color.kt
+++ app/src/main/java/fr/legris/pokedex/ui/theme/Color.kt
@@ -22,73 +22,20 @@ val yellowLight = Color(0xFFFFFE52)
 val yellowDark = Color(0xFFC79B00)
 
 val normalTypeColor = Color(0xFFa8a878)
-val normalTypeColorLight = Color(0xFFe9e9dd)
-val normalTypeColorDark = Color(0xFF2b2b1c)
-
 val fireTypeColor = Color(0xFFf08030)
-val fireTypeColorLight = Color(0xFFfbdfcb)
-val fireTypeColorDark = Color(0xFF431e04)
-
 val waterTypeColor = Color(0xFF6890f0)
-val waterTypeColorLight = Color(0xFFd9e3fb)
-val waterTypeColorDark = Color(0xFF071c4e)
-
 val grassTypeColor = Color(0xFF78c850)
-val grassTypeColorLight = Color(0xFFddf1d3)
-val grassTypeColorDark = Color(0xFF1c3510)
-
 val electricTypeColor = Color(0xFFf8d030)
-val electricTypeColorLight = Color(0xFFfdf3cb)
-val electricTypeColorDark = Color(0xFF6d5804)
-
 val iceTypeColor = Color(0xFF98d8d8)
-val iceTypeColorLight = Color(0xFFe9e9dd)
-val iceTypeColorDark = Color(0xFF2b2b1c)
-
 val fightingTypeColor = Color(0xFFc03028)
-val fightingTypeColorLight = Color(0xFFe9e9dd)
-val fightingTypeColorDark = Color(0xFF2b2b1c)
-
 val poisonTypeColor = Color(0xFFa040a0)
-val poisonTypeColorLight = Color(0xFFe9e9dd)
-val poisonTypeColorDark = Color(0xFF2b2b1c)
-
 val groundTypeColor = Color(0xFFe0c068)
-val groundTypeColorLight = Color(0xFFe9e9dd)
-val groundTypeColorDark = Color(0xFF2b2b1c)
-
 val flyingTypeColor = Color(0xFFa890f0)
-val flyingTypeColorLight = Color(0xFFe9e9dd)
-val flyingTypeColorDark = Color(0xFF2b2b1c)
-
 val psychicTypeColor = Color(0xFFf85888)
-val psychicTypeColorLight = Color(0xFFe9e9dd)
-val psychicTypeColorDark = Color(0xFF2b2b1c)
-
 val bugTypeColor = Color(0xFFa8b820)
-val bugTypeColorLight = Color(0xFFe9e9dd)
-val bugTypeColorDark = Color(0xFF2b2b1c)
-
 val rockTypeColor = Color(0xFFb8a038)
-val rockTypeColorLight = Color(0xFFe9e9dd)
-val rockTypeColorDark = Color(0xFF2b2b1c)
-
 val ghostTypeColor = Color(0xFF705898)
-val ghostTypeColorLight = Color(0xFFe9e9dd)
-val ghostTypeColorDark = Color(0xFF2b2b1c)
-
 val dragonTypeColor = Color(0xFF7038f8)
-val dragonTypeColorLight = Color(0xFFe9e9dd)
-val dragonTypeColorDark = Color(0xFF2b2b1c)
-
 val darkTypeColor = Color(0xFF705848)
-val darkTypeColorLight = Color(0xFFe9e9dd)
-val darkTypeColorDark = Color(0xFF2b2b1c)
-
 val steelTypeColor = Color(0xFFb8b8d0)
-val steelTypeColorLight = Color(0xFFe9e9dd)
-val steelTypeColorDark = Color(0xFF2b2b1c)
-
 val fairyTypeColor = Color(0xFFff9be1)
-val fairyTypeColorLight = Color(0xFFe9e9dd)
-val fairyTypeColorDark = Color(0xFF2b2b1c)
